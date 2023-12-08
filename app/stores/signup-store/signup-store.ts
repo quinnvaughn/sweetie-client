aafa36c9574b0bd7f075c0c5feef8e94b5bf5994
Quinn Vaughn <qvaughn3@gmail.com>
2023-12-08 13:15:26 -0800

    Fixed logic.

--- app/stores/signup-store/signup-store.ts
+++ app/stores/signup-store/signup-store.ts
@@ -3,7 +3,6 @@ import { createJSONStorage, persist } from "zustand/middleware"
 
 type State = {
 	showSignupModal: boolean
-	cleared: boolean
 	clearSignupModal: () => void
 	timesViewedDates: number
 	incrementTimesViewedDates: (isViewer: boolean) => void
@@ -13,18 +12,15 @@ export const signupStore = create(
 	persist<State>(
 		(set, get) => ({
 			timesViewedDates: 0,
-			cleared: false,
 			showSignupModal: false,
-			clearSignupModal: () => set({ showSignupModal: false, cleared: true }),
+			clearSignupModal: () => set({ showSignupModal: false }),
 			incrementTimesViewedDates: (viewer) => {
 				const newValue = viewer ? 0 : get().timesViewedDates + 1
 				set({ timesViewedDates: newValue })
 				if (newValue >= 3) {
-					if (get().cleared) {
-						set({ showSignupModal: false, cleared: false })
-					} else {
-						set({ showSignupModal: true })
-					}
+					set({ showSignupModal: true })
+				} else {
+					set({ showSignupModal: false })
 				}
 			},
 		}),
