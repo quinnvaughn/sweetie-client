9fe507ee7c92ef6f76e8ecfe729bbe31797b32e1
Quinn Vaughn <qvaughn3@gmail.com>
2023-12-17 11:27:58 -0800

    Added link to mobile.

--- app/styled-system/patterns/num-lines.d.ts
+++ app/styled-system/patterns/num-lines.d.ts
@@ -1,21 +1,22 @@
 /* eslint-disable */
-import type { SystemStyleObject, ConditionalValue } from '../types/index';
-import type { Properties } from '../types/csstype';
-import type { PropertyValue } from '../types/prop-type';
-import type { DistributiveOmit } from '../types/system-types';
-import type { Tokens } from '../tokens/index';
+import type { SystemStyleObject, ConditionalValue } from "../types/index"
+import type { Properties } from "../types/csstype"
+import type { PropertyValue } from "../types/prop-type"
+import type { DistributiveOmit } from "../types/system-types"
+import type { Tokens } from "../tokens/index"
 
 export interface NumLinesProperties {
-   lines?: ConditionalValue<number>
+	lines?: ConditionalValue<number>
 }
 
-
-interface NumLinesStyles extends NumLinesProperties, DistributiveOmit<SystemStyleObject, keyof NumLinesProperties > {}
+interface NumLinesStyles
+	extends NumLinesProperties,
+		DistributiveOmit<SystemStyleObject, keyof NumLinesProperties> {}
 
 interface NumLinesPatternFn {
-  (styles?: NumLinesStyles): string
-  raw: (styles?: NumLinesStyles) => SystemStyleObject
+	(styles?: NumLinesStyles): string
+	raw: (styles?: NumLinesStyles) => SystemStyleObject
 }
 
 /** Simplifies the declaration of the number of lines of text */
-export declare const numLines: NumLinesPatternFn;
+export declare const numLines: NumLinesPatternFn
--- app/styled-system/patterns/num-lines.js
+++ app/styled-system/patterns/num-lines.js
@@ -1,19 +1,21 @@
-import { mapObject } from '../helpers.js';
-import { css } from '../css/index.js';
+import { mapObject } from "../helpers.js"
+import { css } from "../css/index.js"
 
 const numLinesConfig = {
-transform(props) {
-  const { lines, ...rest } = props;
-  return {
-    overflow: "hidden",
-    display: "-webkit-box",
-    "-webkit-box-orient": "vertical",
-    "-webkit-line-clamp": lines,
-    ...rest
-  };
-}}
+	transform(props) {
+		const { lines, ...rest } = props
+		return {
+			overflow: "hidden",
+			display: "-webkit-box",
+			"-webkit-box-orient": "vertical",
+			"-webkit-line-clamp": lines,
+			...rest,
+		}
+	},
+}
 
-export const getNumLinesStyle = (styles = {}) => numLinesConfig.transform(styles, { map: mapObject })
+export const getNumLinesStyle = (styles = {}) =>
+	numLinesConfig.transform(styles, { map: mapObject })
 
 export const numLines = (styles) => css(getNumLinesStyle(styles))
 numLines.raw = getNumLinesStyle
